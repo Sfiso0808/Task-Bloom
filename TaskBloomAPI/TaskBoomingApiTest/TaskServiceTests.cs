7674e953648f294ab67274fc6fc6ccd8bb33c466
sfiso0808 <ww.sfiso.ndzobo@gmail.com>
2025-05-01 16:31:49 +0200

    Update project files and add new test projects

---
+++ TaskBloomAPI/TaskBoomingApiTest/TaskServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+using TaskBloomAPI.Services;
+using TaskBloomAPI.Models;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace TaskBoomingApiTest
+{
+    public class TaskServiceTests
+    {
+        private readonly TaskService _taskService;
+
+        public TaskServiceTests()
+        {
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string>
+                {
+                    { "ConnectionStrings:DefaultConnection", "your_mysql_connection_string_here" }
+                })
+                .Build();
+
+            _taskService = new TaskService(config);
+        }
+
+        [Fact]
+        public void GetAllTasks_ReturnsList()
+        {
+            var result = _taskService.GetAllTasks();
+            Assert.NotNull(result);
+            Assert.IsType<List<TaskBloomAPI.Models.Task>>(result);
+
+        }
+    }
+}
---
+++ TaskBloomingClient/TaskBloomAPI.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskBloomAPI.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
